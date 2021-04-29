90244aa0dbbbce84237109cdb4114258ae6fefee
romanpalenik <brzdatv@gmail.com>
2021-04-29 17:07:59 +0200

    Opraveny merge

--- src/project/controller/UseFullLinksController.java
+++ src/project/controller/UseFullLinksController.java
@@ -176,6 +176,7 @@ public class UseFullLinksController extends AplicationWindow implements Internat
         linkDatabase.addGroupLink(newLinkGroup.getText());
         linkView.showGroupLinks();
         linkDatabase.safeEvents();
+
     }
 
     @FXML
